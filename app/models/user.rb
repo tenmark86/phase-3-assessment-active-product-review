c0177b77164ee63e4dc8a947cfbd67076027681e
tenmark86e <tenmark86@gmail.com>
2022-11-05 22:26:11 +0300

    added code for user file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def favorite_product
+        self.reviews.all.sort{ |a,b| a.star_rating <=> b.star_rating }.last
+    end
+
+    def remove_reviews(product)
+        self.products.find_by(id: product.id).reviews.destroy_all
+    end
 end
